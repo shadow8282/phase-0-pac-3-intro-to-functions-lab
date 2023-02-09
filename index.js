8acceb65103ff2017bdf910412a977d6a6e5ce34
Cayden <Caydenburr@gmail.com>
2023-02-08 18:58:44 -0700

    complete lab

---
+++ index.js
@@ -0,0 +1,33 @@
+function shout(string){
+
+    return string.toUpperCase();
+};
+
+function whisper(string){
+    return string.toLowerCase();
+};
+
+function logShout(string){
+    console.log(shout(string));
+};
+
+function logWhisper(string){
+    console.log(whisper(string));
+};
+
+function sayHiToHeadphonedRoommate(string){
+    if(string === "hello"){
+        return "I can\'t hear you!";
+    }
+    else if (string === "HELLO"){
+        return "YES INDEED!";
+    }
+    else if (string === "Let\'s have dinner together!"){
+        return  "I would love to!";
+    };
+};
+
+shout("hello");
+whisper("HELLO");
+logShout("hello");
+logWhisper("HELLO");
